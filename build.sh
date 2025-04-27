ca7c36664f5488843d4996980adc85d5f1a7d126
suhong99 <topic828@naver.com>
2025-04-27 14:48:03 +0000

    무료배포

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd ../
+mkdir output
+cp -R ./backoffice/* ./output
+cp -R ./output ./backoffice/
---
+++ src/auth.ts
@@ -0,0 +1,24 @@
+import NextAuth, { NextAuthConfig } from 'next-auth';
+import Google from 'next-auth/providers/google';
+
+export const authConfig = {
+	pages: {
+		signIn: '/',
+	},
+	theme: { logo: 'https://authjs.dev/img/logo-sm.png' },
+	callbacks: {
+		authorized({ auth, request: { nextUrl } }) {
+			const isLoggedIn = !!auth?.user;
+			const openedPath = ['/'];
+			const isOpened = openedPath.includes(nextUrl.pathname);
+			if (!isOpened) {
+				if (isLoggedIn) return true;
+				return Response.redirect(new URL('/', nextUrl));
+			}
+			return true;
+		},
+	},
+	providers: [Google],
+} satisfies NextAuthConfig;
+
+export const { handlers, signIn, signOut, auth } = NextAuth(authConfig);
