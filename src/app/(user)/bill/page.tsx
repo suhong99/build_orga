d2c7ab2d31f7acdd42134efd0eab2b696053fb3b
suhong99 <topic828@naver.com>
2025-06-19 07:50:51 +0000

    feat:임시배너

--- src/app/(user)/bill/page.tsx
+++ src/app/(user)/bill/page.tsx
@@ -9,7 +9,9 @@ export const metadata: Metadata = {
 export default function BillPage() {
 	return (
 		<>
-			<div className="w-full h-[250px] flex items-center justify-center  max-w-maxw bg-primary-main-strong">배너 </div>
+			<div className="w-full h-[150px] min-desktop:w-[100vw] flex items-center justify-center max-w-maxw bg-[#118D65] text-white typo-title2 desktop:rounded-[20px] desktop:my-10 desktop:typo-title1 desktop:font-bold	text-center">
+				최근에 발의된 <br className="desktop:hidden" /> 법안을 확인해보세요
+			</div>
 			<div className="w-full flex flex-col flex-1 gap-16 desktop:gap-12 items-center px-9 py-12 bg-bg-gray ">
 				<QueryBar />
 				<BillContents />
--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -4,7 +4,10 @@ import ContentHeader from '@/features/home/ContentHeader';
 export default function Home() {
 	return (
 		<>
-			<div className="w-full h-[250px] flex items-center justify-center  max-w-maxw bg-primary-main-strong">배너 </div>
+			<div className="w-full h-[150px] min-desktop:w-[100vw] flex items-center justify-center max-w-maxw bg-[#F0ECFE] typo-title2 desktop:rounded-[20px] desktop:my-10 desktop:typo-title1 desktop:font-bold	text-center">
+				나의 색을 찾아서,
+				<br className="desktop:hidden" /> 그래 이 픽!
+			</div>
 			<div className="w-full flex flex-col flex-1 gap-16 desktop:gap-12 items-center px-9 py-12 bg-bg-gray ">
 				<Content>
 					<ContentHeader title="누구님이 관심있는" keywordList={['정치', '피그마', '회의']} link="/issue" />
