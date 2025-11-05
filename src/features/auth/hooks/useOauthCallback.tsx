8f8f6b99389caedded67d2117d15c7e8b154a517
suhong99 <topic828@naver.com>
2025-11-05 10:26:14 +0000

    fix: 외부브라우저 열렸을 경우 deeplink 복귀 로직 추가

--- src/features/auth/hooks/useOauthCallback.tsx
+++ src/features/auth/hooks/useOauthCallback.tsx
@@ -12,6 +12,13 @@ export const useOauthCallback = (type: OAuthType) => {
 		const code = url.searchParams.get('code');
 		if (!code) return;
 
+		// ✅ 일반 브라우저 팝업 환경
+		if (window.opener) {
+			window.opener.postMessage({ type: 'OAUTH_CODE', code }, '*');
+			window.close?.();
+			return;
+		}
+
 		// ✅ RN WebView 환경 (ReactNativeWebView 존재)
 		if (window.ReactNativeWebView) {
 			try {
@@ -59,11 +66,25 @@ export const useOauthCallback = (type: OAuthType) => {
 			return;
 		}
 
-		// ✅ 일반 브라우저 팝업 환경
-		if (window.opener) {
-			window.opener.postMessage({ type: 'OAUTH_CODE', code }, '*');
-			window.close?.();
-			return;
-		}
+		// 웹이나 rn내부가 아닌경우는 외부 브라우저 -> 딥링크로 복귀 시도
+		const deeplinkUrl = `com.graypick.co.kr://callback?code=${code}`;
+		let opened = false;
+
+		const handleBlur = () => {
+			opened = true; // 앱으로 전환됨
+			clearTimeout(timer);
+			window.removeEventListener('blur', handleBlur);
+		};
+
+		window.addEventListener('blur', handleBlur);
+
+		const timer = setTimeout(() => {
+			window.removeEventListener('blur', handleBlur);
+			if (!opened) {
+				alert('앱이 설치되어 있지 않아요. 앱에서 다시 시도해주세요.');
+			}
+		}, 1000);
+
+		window.location.href = deeplinkUrl;
 	}, [queryClient, router, type]);
 };
